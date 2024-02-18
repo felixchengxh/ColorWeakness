4339e9f013acb31131a638a4725e6f40f486c14f
Felix <2030660534@qq.com>
2024-02-18 20:42:33 +0800

    Add FPS option

--- scripts/Image2Video.py
+++ scripts/Image2Video.py
@@ -5,10 +5,12 @@ import os
 parser = argparse.ArgumentParser(description='Demo of argparse')
 parser.add_argument('--input_dir', default='./tmpFramesResult')
 parser.add_argument('--output_video', default='./VideoOutput/')
+parser.add_argument('--FPS', type=int, default=30)
 if __name__ == "__main__":
     args = parser.parse_args()
     source_path = args.input_dir
     output_path = args.output_video
+    FPS = args.FPS
     if not os.path.exists(output_path):
         os.mkdir(output_path)
     file_list = os.listdir(source_path)
@@ -19,7 +21,7 @@ if __name__ == "__main__":
     height, width, layers = frame.shape
     # 使用cv2.VideoWriter()创建视频编写器
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # 或者使用其他视频编解码器例如'XVID'
-    video = cv2.VideoWriter(os.path.join(output_path, '1.mp4'), fourcc, 30, (int(width), int(height)))
+    video = cv2.VideoWriter(os.path.join(output_path, '1.mp4'), fourcc, FPS, (int(width), int(height)))
 
     # 循环读取图片并写入视频
     for i in range(num_files):
